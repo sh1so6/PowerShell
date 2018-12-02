fd60b5462b4d843479c57ceb4dd3348a2f290739
sh1so6 <jade.shisonoha@gmail.com>
2018-12-02 21:54:18 +0900

    Add Search Form and Button

--- searchFiles.ps1
+++ searchFiles.ps1
@@ -3,6 +3,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 # $EF = New-Object ([System.IO.SearchOption]::AllDirectories)
 
+# 引数値でディレクトリ内検索を行う
 function SearchDirectories {
     #[CmdletBinding()]
     param (
@@ -32,6 +33,8 @@ function SearchDirectories {
         $ErrorActionPreference = "Stop"
     }
 }
+
+# 検索結果ビュー作成
 function CreateListView {
     $_ListView = New-Object System.Windows.Forms.ListView
     # スタイル設定
@@ -42,8 +45,8 @@ function CreateListView {
     $cName.Width = 500
     $cType = New-Object System.Windows.Forms.ColumnHeader
     $cType.Text = "ファイル/フォルダ"
-    $cName.Width = 100
-    [System.Windows.Forms.ColumnHeader[]]$c = $cName,$cType
+    $cType.Width = 100
+    [System.Windows.Forms.ColumnHeader[]]$c = ($cName,$cType)
     # ヘッダー設定
     $_ListView.Columns.AddRange($c)
     # 並び替えの許可
@@ -51,14 +54,120 @@ function CreateListView {
     # グリッド線の許可
     $_ListView.GridLines = $true
 
+    # Anchor設定
+    # $_ListView.Anchor = [System.Windows.Forms.AnchorStyles]::Right -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Bottom
+    # Dock設定
+    $_ListView.Dock = [System.Windows.Forms.DockStyle]::Fill
 
     return $_ListView
 }
 
-$form = New-Object System.Windows.Forms.Form
-$listView = CreateListView
-$listView.Items.Add("aaa")
-$form.Controls.Add($listView)
+# 検索パス入力ボックス作成
+function CreatePathBox {
+    # [CmdletBinding()]
+    param (
+        
+    )
+    begin {
+        $ErrorActionPreference = "SilentlyContinue"
+    }
+    process {
+        $_textBox = New-Object System.Windows.Forms.TextBox
+        # Dock設定
+        $_textBox.Dock = [System.Windows.Forms.DockStyle]::Top
+
+        return $_textBox
+    }
+    end {
+        $ErrorActionPreference = "Stop"
+    }
+}
+
+# 検索パス入力ボックスラベル作成
+function CreatePathLabel {
+    # [CmdletBinding()]
+    param (
+
+    )
+    begin {
+        $ErrorActionPreference = "SilentlyContinue"
+    }
+    process {
+        $_label = New-Object System.Windows.Forms.Label
+        $_label.Text = "検索対象パス"
+        # Dock設定
+        $_label.Dock = [System.Windows.Forms.DockStyle]::Top
+
+        return $_label
+    }
+    end {
+        $ErrorActionPreference = "Stop"
+    }
+}
+
+# 検索ボタン作成
+function CreateSearchButton {
+    # [CmdletBinding()]
+    param (
+        
+    )
+    begin {
+        $ErrorActionPreference = "SilentlyContinue"
+    }
+    process {
+        $_button = New-Object System.Windows.Forms.Button
+        $_button.Text = "検索"
+
+        # Dock設定
+        $_button.Dock = [System.Windows.Forms.DockStyle]::Top
+
+        return $_button
+    }
+    end {
+        $ErrorActionPreference = "Stop"
+    }
+}
+
+# 追加アイテム作成
+function CreateItem {
+    # [CmdletBinding()]
+    param (
+        [string]$path
+    )
+    begin {
+        $ErrorActionPreference = "SilentlyContinue"
+    }
+    process {
+        $_item = New-Object System.Windows.Forms.ListViewItem($path)
+        return $_item
+    }
+    end {
+        $ErrorActionPreference = "Stop"
+    }
+}
+
+# メインウィンドウの作成
+function CreateMainWindow {
+    $_form = New-Object System.Windows.Forms.Form
+
+    # 検索結果ビュー作成
+    $listView = CreateListView
+    $_form.Controls.Add($listView)
+    # 検索パス入力ボックス追加
+    $pathBox = CreatePathBox
+    $_form.Controls.Add($pathBox)
+    # 検索パス入力ラベル追加
+    $pathLabel = CreatePathLabel
+    $_form.Controls.Add($pathLabel)
+    # 検索ボタン追加
+    $searchButton = CreateSearchButton
+    $_form.Controls.Add($searchButton)
+
+    return $_form
+}
+
+# Main
+$form = CreateMainWindow
 # 最前面表示
 $form.TopMost = $true
 $res = $form.ShowDialog()
